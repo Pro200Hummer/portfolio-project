1729aec29462f4d14c8981a613df11a456fbc880
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-10-03 19:04:39 +0300

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {Header} from "./components/Header/Header";
+import style from './commonStyles/Common.module.scss'
 
 export const App = () => {
-  return (
-    <>
-    </>
-  );
+    return (
+        <div className={style.wrapper}>
+            <div className={style.content}>
+                <Header/>
+            </div>
+        </div>
+    )
 };
