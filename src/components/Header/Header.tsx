f707aaedc4d181bb6545ade2e41a7a6ce2125e40
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-10-03 19:56:01 +0300

    Refactor Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,17 +5,12 @@ import style from './Header.module.scss'
 export const Header: FC = () => {
     return (
         <header className={style.container}>
-            <div className={style.row}>
-                <div className={style.logo}>
-                    L
-                </div>
-                <nav className={style.nav}>
-                    <a href="#" className={`${style.link} ${style.btnNav}`}>Main</a>
-                    <a href="#" className={`${style.link} ${style.btnNav}`}>Skills</a>
-                    <a href="#" className={`${style.link} ${style.btnNav}`}>Projects</a>
-                    <a href="#" className={`${style.link} ${style.btnNav}`}>Contacts</a>
-                </nav>
-            </div>
+            <nav className={style.nav}>
+                <a href="#" className={`${style.link} ${style.btnNav}`}>Main</a>
+                <a href="#" className={`${style.link} ${style.btnNav}`}>Skills</a>
+                <a href="#" className={`${style.link} ${style.btnNav}`}>Projects</a>
+                <a href="#" className={`${style.link} ${style.btnNav}`}>Contacts</a>
+            </nav>
         </header>
     )
 };
