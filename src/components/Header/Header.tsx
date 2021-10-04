dfd0d9d74b74febb74e073ea8f154c6bb7a5ff25
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-10-04 14:28:27 +0300

    Refactor Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,21 @@ export const Header: FC = () => {
                 <a href="#" className={`${style.link} ${style.btnNav}`}>Projects</a>
                 <a href="#" className={`${style.link} ${style.btnNav}`}>Contacts</a>
             </nav>
+            <div>line</div>
+            <div className={style.info}>
+                <h1>Pavel Nikalaychik</h1>
+                <p>
+                    Front-end developer<br/>
+                    26 years old, Minsk
+                </p>
+                <div className={style.lang}>
+                    <div>
+                        <span className={style.langRu}>ru</span><b>|</b>
+                        <span className={style.langRu}><b>eng</b></span>
+                    </div>
+                </div>
+                <img className={style.photo} src="#" alt="#"/>
+            </div>
         </header>
     )
 };
